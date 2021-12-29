d653773d6f1b88008355a30239fbf2ca6d8bc716
VladTheJunior <vladthejunior@gmail.com>
2021-12-29 18:18:29 +0800

    Updated Discord

--- js/discord-widget.js
+++ js/discord-widget.js
@@ -1,8 +1,3 @@
-function declOfNum(number, titles) {  
-    cases = [2, 0, 1, 1, 1, 2];  
-    return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
-}
-
 document.addEventListener("DOMContentLoaded", function() { 
     var requestURL = 'https://discordapp.com/api/guilds/739033277301522433/widget.json';
     var request = new XMLHttpRequest();
@@ -11,6 +6,6 @@ document.addEventListener("DOMContentLoaded", function() {
     request.send();
     request.onload = function () {
         var discord = request.response;
-        document.querySelector('.discord-invite-header-count').innerHTML = '<div class="discord-invite-header-online-status"></div>' + discord['presence_count'] + ' в сети' + '<div class="discord-invite-header-overall-status"></div>' + discord['members'].length + declOfNum(discord['members'].length, ['участник', 'участника', 'участников']);
+        document.querySelector('.discord-invite-header-count').innerHTML = '<div class="discord-invite-header-online-status"></div>' + discord['presence_count'] + ' в сети';
     }
 });

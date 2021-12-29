76c84ff6629af41b66b0438c7d42c7b09e3a8727
VladTheJunior <vladthejunior@gmail.com>
2021-12-29 18:05:11 +0800

    Updated Discord

--- js/discord-widget.js
+++ js/discord-widget.js
@@ -11,6 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
     request.send();
     request.onload = function () {
         var discord = request.response;
-        document.querySelector('.discord-invite-header-count').innerHTML = '<div class="discord-invite-header-online-status"></div>' + discord['presence_count'] + ' в сети' + '<div class="discord-invite-header-overall-status"></div>' + discord['members'].length + declOfNum(discord['members'].count(), ['участник', 'участника', 'участников']);
+        document.querySelector('.discord-invite-header-count').innerHTML = '<div class="discord-invite-header-online-status"></div>' + discord['presence_count'] + ' в сети' + '<div class="discord-invite-header-overall-status"></div>' + discord['members'].length + declOfNum(discord['members'].length, ['участник', 'участника', 'участников']);
     }
 });
